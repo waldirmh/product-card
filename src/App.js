847d61fde30131c8baa7746fea994d79bedc31c9
waldirMh <waldirmendozahuaman887@gmail.com>
2025-08-08 15:07:33 -0500

    fix:init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className='section'>
+      <div className="content">
+        <div className="card">
+          <img src="/image-product-desktop.jpg" alt="image card" />
+          <div className="description">
+            <h4>Perfume</h4>
+            <h1>Gabrielle Essence Eau De Parfum</h1>
+            <p className="detail">
+              A floral, solar and voluptuous interpretation composed by Olivier
+              Polge, perfumer-Creator for the House of CHANEL
+            </p>
+            <p className="price">
+              <span>$149.99</span>
+              <span>$169.99</span>
+            </p>
+            <button className="primary-button add-to-cart-button">
+              <img src="/icon-cart.svg" alt="Add to Cart" />
+              Add to Cart
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
